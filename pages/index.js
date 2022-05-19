23d3e1789b4db98030d6f618a6a268e053ec01a3
Jesus Juvinao <53837840+Jesus123780@users.noreply.github.com>
2022-05-19 16:57:20 -0500

    upload test 6

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Papulito Jesus Juvinao  cambio rama protegida prueba 5  <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Papulito Jesus Juvinao  cambio rama protegida prueba 6  <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
