e014f746a0f198549f8ec58074b0179ff7d0b2bf
Daniel Betancur Rodríguez <danielbetancur@ingenieros.com>
2022-05-19 14:48:47 -0500

    cambio de Daniel

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Este es mi cambio (Daniel)
         </h1>
 
         <p className={styles.description}>
